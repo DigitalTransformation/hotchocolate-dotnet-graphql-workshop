2850b8cda33041f86da0ea6410d7f2d07cf5b805
Sar Malik <malik@sarthak.ca>
2020-11-09 11:05:00 -0500

    ApiContext: Override OnConfiguring() UseNpgSql

--- GraphQL/Data/ApiContext.cs
+++ GraphQL/Data/ApiContext.cs
@@ -3,30 +3,33 @@ namespace GraphQL.Data
     using System;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using Npgsql;
 
     public class ApiContext : DbContext
     {
+        private readonly ILogger<ApiContext> _logger;
+
         private static IConfiguration _configuration =>
             Startup.Configuration;
 
-        private static IConfigurationSection _section =>
+        private static IConfigurationSection _dbSection =>
             _configuration.GetSection("DataBinding:Postgres");
 
-        public ApiContext(DbContextOptions<ApiContext> options)
+        private static string _connectionStr;
             : base(options)
         {
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connection = _section["Default"];
-
+            _logger.LogInformation($"Using connectionStr: {_connectionStr}");
             optionsBuilder
-                .UseNpgsql(connection);
+                .UseNpgsql(new NpgsqlConnection(_connectionStr));
 
-            if (!optionsBuilder.IsConfigured)
+            if (!optionsBuilder.IsConfigured || string.IsNullOrEmpty(_connectionStr))
             {
-                Console.WriteLine("[ApiContext]: Could not configure DbContext provider method. Check connectionString or method properties instantiated.");
+                _logger.LogInformation("Could not configure DbContext provider method. Check connectionString or method properties instantiated.");
             }
 
             base.OnConfiguring(optionsBuilder);
