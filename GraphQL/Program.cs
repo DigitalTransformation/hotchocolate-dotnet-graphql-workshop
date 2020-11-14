ccdf6880f9554476743c13ca6f7a193595cb5a66
Sar Malik <malik@sarthak.ca>
2020-11-14 13:00:00 -0500

    Program:refactor impl ConfigureServices AddHostedService<TimeService>

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -1,26 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 namespace GraphQL
 {
-    public class Program
+    using GraphQL.Service;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public static class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+                .ConfigureServices((_, services) =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    services
+                        .AddHostedService<TimeService>();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder.UseStartup<Startup>());
     }
 }
