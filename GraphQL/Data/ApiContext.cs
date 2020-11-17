698a3e8ad0a50d62a5ffd2715809db25d90d01b2
Sar Malik <malik@sarthak.ca>
2020-11-17 12:55:00 -0500

    ApiContext impl new prop types: Session, Track, Attendee

--- GraphQL/Data/ApiContext.cs
+++ GraphQL/Data/ApiContext.cs
@@ -65,6 +65,30 @@ namespace GraphQL.Data
             base.OnConfiguring(optionsBuilder);
         }
 
-        public DbSet<Speaker> Speakers { get; set; }
+        public DbSet<Session> Sessions { get; set; } = default!;
+
+        public DbSet<Track> Tracks { get; set; } = default!;
+
+        public DbSet<Speaker> Speakers { get; set; } = default!;
+
+        public DbSet<Attendee> Attendees { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Attendee>()
+                .HasIndex(a => a.UserName)
+                .IsUnique();
+
+            // Many-to-many: Session <-> Attendee
+            modelBuilder
+                .Entity<SessionAttendee>()
+                .HasKey(ca => new { ca.SessionId, ca.AttendeeId });
+
+            // Many-to-many: Speaker <-> Session
+            modelBuilder
+                .Entity<SessionSpeaker>()
+                .HasKey(ss => new { ss.SessionId, ss.SpeakerId });
+        }
     }
 }
